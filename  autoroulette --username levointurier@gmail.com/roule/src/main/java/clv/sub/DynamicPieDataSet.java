92257708c54917dd10b173f7d10d7b96c3e2ff88
levointurier@gmail.com <levointurier@gmail.com@21b5c50d-459f-00a2-b917-5ce169890a32>
2013-05-16 23:28:35 +0000

    names in log

---  autoroulette --username levointurier@gmail.com/roule/src/main/java/clv/sub/DynamicPieDataSet.java
+++  autoroulette --username levointurier@gmail.com/roule/src/main/java/clv/sub/DynamicPieDataSet.java
@@ -13,11 +13,12 @@ public class DynamicPieDataSet extends DefaultPieDataset {
 	private ArrayList<Integer> rawdata = new ArrayList<Integer>();
 	private HashMap<String, Integer> data = new HashMap<String, Integer>();
 	private HashMap<Integer, String> labels = new HashMap<Integer, String>();
-
+	private String title;
 	private int delta = 1;
 
-	public DynamicPieDataSet() {
+	public DynamicPieDataSet(String _title) {
 		super();
+		title = _title;
 		reset(0);
 	}
 
@@ -54,7 +55,7 @@ public class DynamicPieDataSet extends DefaultPieDataset {
 		data.clear();
 		labels.clear();
 		clear();
-		System.out.print("\n--- Reset for " + valToPut + ":delta=" + delta + " labels:");
+		System.out.print("\n--- " + title + ": Reset for " + valToPut + ":delta=" + delta + " labels:");
 		for (Integer iip = 0; iip < NBR_PARTS; iip++) {
 			String lab = (iip * delta) + "-" + (((iip + 1) * delta - 1));
 			if (delta == 1) {
@@ -64,10 +65,14 @@ public class DynamicPieDataSet extends DefaultPieDataset {
 			labels.put(iip, lab);
 			data.put(labels.get(iip), 0);
 		}
-		System.out.print(" --- \n refilling " + rawdata.size() + " values ");
+		System.out.print(" ---  refilling " + rawdata.size() + " values ");
 		for (Integer ii : rawdata) {
 			addInData(ii / delta);
 		}
-		System.out.print("done.");
-		}
+		System.out.println("done.");
+	}
+
+	public String getTitle() {
+		return title;
+	}
 }
