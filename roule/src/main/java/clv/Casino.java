c3c1aeac01be739919ca881d650a8513615b147e
levointurier <levointurier@21b5c50d-459f-00a2-b917-5ce169890a32>
2013-10-21 06:08:53 +0000

    v2

--- roule/src/main/java/clv/Casino.java
+++ roule/src/main/java/clv/Casino.java
@@ -1,20 +1,16 @@
 package clv;
 
-import clv.view.CroupierView;
-import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 
-public class Casino extends JFrame {
+public class Casino {
 
     private static final long serialVersionUID = 2597779237651500313L;
-     public static JProgressBar bar = new JProgressBar();
-
+    public static JProgressBar bar = new JProgressBar();
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        new CroupierView();
+        new Croupier(true);
     }
-
 }
